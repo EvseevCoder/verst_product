ce9046efbb487cde7236ee7b024d46edd32fc5ae
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-10-01 23:43:43 +0300

    Создание dom_test

--- js/Dom_test/script.js
+++ js/Dom_test/script.js
@@ -10,7 +10,7 @@ uName = document.getElementById('user_write');
 // 1 проверка
 if (localStorage.getItem('uName') == null) {
     // 2
-    uName.textContent = prompt("Введите свой никнейм");
+    uName.textContent = 'User';
     localStorage.setItem('uName', uName.textContent)
 } else {
     uName.textContent = localStorage.getItem('uName');
@@ -19,7 +19,7 @@ if (localStorage.getItem('uName') == null) {
 
 let del_name = document.getElementById('del_name')
 
-del_name.onclick = function() {
+del_name.onclick = function () {
 
     console.log("Ваше имя удалено");
 
@@ -29,11 +29,38 @@ del_name.onclick = function() {
 
 let edit_name = document.getElementById('edit_name')
 
-edit_name.onclick = function() {
+edit_name.onclick = function () {
 
     uName.textContent = prompt('Введите ваше имя');
     if (uName.textContent == '') {
         uName.textContent = "User"
     }
+
     localStorage.setItem('uName', uName.textContent)
+}
+
+
+// 1. По нажатию кнопки запраищвается позиция пользователя
+// 2. После подтверждения позиции скрывается блок с кнопкой
+// 3. Открывается блок с широтой и высотой
+
+
+let getGeo = document.getElementById('get-geo')
+let latitude = document.getElementById('latitude')
+let longitude = document.getElementById('longitude')
+let zapros = document.getElementById('zapros')
+let posit = document.getElementById('posit')
+
+getGeo.onclick = function () {
+
+    navigator.geolocation.getCurrentPosition(function (geo) {
+        latitude.textContent = geo.coords.latitude
+        longitude.textContent = geo.coords.longitude
+    })
+
+    zapros.classList.add('hide-block')
+    zapros.classList.remove('show-block')
+
+    posit.classList.add('show-block')
+    posit.classList.remove('hide-block')
 }
