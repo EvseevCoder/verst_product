ccd857957c6b67d75df925467807029376e9bf36
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-10-07 14:31:53 +0300

    Добавление API Dogs? Пока криво-косо, но работает

--- js/шаблонизация/script.js
+++ js/шаблонизация/script.js
@@ -28,7 +28,34 @@ button1.onclick = function () {
     content.innerHTML += TakeObejct(photo[0])
     content.innerHTML += TakeObejct(photo[0])
     content.innerHTML += TakeObejct(photo[0])
-
-    console.log(123);
 }
 
+let addDog = document.querySelector('.add-dog')
+
+
+
+let url1 = 'https://dog.ceo/api/breeds/image/random';
+
+// Функция моя, но не работает по непонятным причинам
+let TakeDog = async function (url, content) {
+
+    let response = await fetch(url)
+    let json = await response.json()
+
+    content.innerHTML += `
+    <div class="dog">
+            <img src="${json.message}" alt="">
+    </div>
+    `
+
+    console.log(json.message);
+
+    return json
+}
+
+console.log(TakeDog(url1, content));
+
+addDog.onclick = function () {
+    TakeDog(url1, content)
+    console.log(123);
+}
